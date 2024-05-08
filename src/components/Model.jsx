88f4d37de96499f34b889773ec29735df9080d1e
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-05-07 23:15:27 -0500

    2:50

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,32 +1,52 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
+
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animation";
 
 const Model = () => {
   const [size, setSize] = useState("small");
   const [model, setModel] = useState({
-    title: "iPhone 15 Pro in Titanium.",
-    color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
     img: yellowImg,
   });
 
-  // camera control
+  // camera control for the model view
   const cameraControlSmall = useRef();
   const cameraControlLarge = useRef();
 
-  // models
+  // model
   const small = useRef(new THREE.Group());
   const large = useRef(new THREE.Group());
 
   // rotation
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+
+    if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
+    }
+  }, [size]);
 
   useGSAP(() => {
     gsap.to("#heading", { y: 0, opacity: 1 });
@@ -38,6 +58,7 @@ const Model = () => {
         <h1 id="heading" className="section-heading">
           Take a closer look.
         </h1>
+
         <div className="flex flex-col items-center mt-5">
           <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
             <ModelView
@@ -45,24 +66,27 @@ const Model = () => {
               groupRef={small}
               gsapType="view1"
               controlRef={cameraControlSmall}
-              setRotationSate={setSmallRotation}
+              setRotationState={setSmallRotation}
               item={model}
               size={size}
             />
+
             <ModelView
               index={2}
               groupRef={large}
               gsapType="view2"
               controlRef={cameraControlLarge}
-              setRotationSate={setLargeRotation}
+              setRotationState={setLargeRotation}
               item={model}
               size={size}
             />
+
             <Canvas
               className="w-full h-full"
               style={{
                 position: "fixed",
                 top: 0,
+                bottom: 0,
                 left: 0,
                 right: 0,
                 overflow: "hidden",
@@ -72,27 +96,29 @@ const Model = () => {
               <View.Port />
             </Canvas>
           </div>
+
           <div className="w-full mx-auto">
-            <p className="pb-5 text-sm font-light text-center">{model.title}</p>
+            <p className="mb-5 text-sm font-light text-center">{model.title}</p>
+
             <div className="flex-center">
               <ul className="color-container">
-                {models.map((item, index) => (
+                {models.map((item, i) => (
                   <li
-                    key={index}
+                    key={i}
                     className="w-6 h-6 mx-2 rounded-full cursor-pointer"
-                    style={{ background: item.color[0] }}
+                    style={{ backgroundColor: item.color[0] }}
                     onClick={() => setModel(item)}
                   />
                 ))}
               </ul>
+
               <button className="size-btn-container">
                 {sizes.map(({ label, value }) => (
                   <span
                     key={label}
-                    className=" size-btn"
+                    className="size-btn"
                     style={{
-                      backgroundColor:
-                        size === value ? "white " : "transparent",
+                      backgroundColor: size === value ? "white" : "transparent",
                       color: size === value ? "black" : "white",
                     }}
                     onClick={() => setSize(value)}
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -4,17 +4,19 @@ import {
   PerspectiveCamera,
   View,
 } from "@react-three/drei";
-import Lights from "./Lights";
-import { Suspense } from "react";
-import IPhone from "./IPhone";
+
 import * as THREE from "three";
+import Lights from "./Lights";
+import Loader from "./Loader";
+import IPhone from "./IPhone";
+import { Suspense } from "react";
 
 const ModelView = ({
   index,
   groupRef,
   gsapType,
   controlRef,
-  setRotationSize,
+  setRotationState,
   size,
   item,
 }) => {
@@ -22,32 +24,31 @@ const ModelView = ({
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full  ${index === 2} ? right-[100%]: ""`}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
     >
+      {/* Ambient Light */}
       <ambientLight intensity={0.3} />
+
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
       <Lights />
+
       <OrbitControls
+        makeDefault
         ref={controlRef}
         enableZoom={false}
         enablePan={false}
-        makeDefault
-        rotateSpeed={0.5}
+        rotateSpeed={0.4}
         target={new THREE.Vector3(0, 0, 0)}
-        onEnd={() => setRotationSize(controlRef.current.getAzimuthalAngle())}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
       />
+
       <group
         ref={groupRef}
-        name={`${index === 1} ? "small" : "large"`}
+        name={`${index === 1} ? 'small' : 'large`}
         position={[0, 0, 0]}
       >
-        <Suspense
-          fallback={
-            <Html>
-              <div>Loading</div>
-            </Html>
-          }
-        >
+        <Suspense fallback={<Loader />}>
           <IPhone
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
